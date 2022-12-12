47f3107b26bb9d7ff6b1d70e7d38afb2b1a0933c
Kin Yuunan <yuunan.kin@gmail.com>
2022-12-13 08:17:07 +0900

    handle request body

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -7,11 +7,15 @@ const openai = new OpenAIApi(configuration);
 
 // more usage see: https://beta.openai.com/docs/guides/images/usage
 const generateImage = async (req, res) => {
+  const { text, size } = req.body;
+  const imageSize =
+    size == "small" ? "256x256" : size == "medium" ? "512x512" : "1024x1024";
+  console.log(`Generating "${text}" at ${size}`);
   try {
     const response = await openai.createImage({
-      prompt: "naruto",
+      prompt: text,
       n: 1,
-      size: "512x512",
+      size: imageSize,
     });
     const imageUrl = response.data.data[0].url;
     res.status(200).json({ message: "ok", data: imageUrl });
